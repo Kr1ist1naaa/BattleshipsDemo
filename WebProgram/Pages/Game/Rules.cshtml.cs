a7420ff31ac475b15dd0b49f961dc23207fbc38b
siegrest <179900@ttu.ee>
2018-12-18 15:25:22 +0200

    Working rule editor

--- WebProgram/Pages/Game/Rules.cshtml.cs
+++ WebProgram/Pages/Game/Rules.cshtml.cs
@@ -1,45 +1,38 @@
+using Domain.DomainRule;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebProgram.Pages.Game {
     public class RulesModel : PageModel {
-        public string MainTitle { get; private set; } = "Changing rules for new game";
-        public string ResetBtnTitle { get; private set; } = "Reset all rules";
-        public string BackBtnHref { get; private set; } = "New";
+        public string MainTitle { get; } = "Rules for new game";
+        public string BackBtnHref { get; } = "/Game/New";
+        public string StatusMsg { get; private set; } = "[Status message]";
 
-        public bool HasAction { get; private set; }
-        public bool HasType { get; private set; }
-        public bool GenRulesTable { get; private set; }
-        public bool IsReset { get; private set; }
-        public bool IsSave { get; private set; }
+        public bool IsAction { get; private set; }
+       
+        public void OnPost() {
+            IsAction = true;
+            StatusMsg = "Rules saved!";
 
-        public string Action { get; private set; }
-        public string Type { get; private set; }
+            foreach (var key in Request.Form.Keys) {
+                // Skip empty parameters
+                if (!Request.Form.TryGetValue(key, out var strVal)) {
+                    continue;
+                }
+
+                // Skip non-integer parameters
+                if (!int.TryParse(key, out var intKey)) continue;
+                if (!int.TryParse(strVal, out var intVal)) continue;
+
+                // Update static rule context
+                Rules.ChangeRule((RuleType) intKey, intVal);
+            }
+        }
 
         public void OnGet() {
-            HasAction = Request.Query.TryGetValue("action", out var action);
-            HasType = Request.Query.TryGetValue("type", out var type);
-
-            Action = action.ToString().ToLower().Trim();
-            Type = type.ToString().ToLower().Trim();
-
-            if (HasAction && HasType) {
-                BackBtnHref = "Rules";
-                
-                MainTitle = $"Changing {Type} rules";
-                ResetBtnTitle = $"Reset {Type} rules";
-                    
-                if (Action.Equals("list")) {
-                    if (Type.Equals("general") || Type.Equals("size") || Type.Equals("count")) {
-                        GenRulesTable = true;
-                    } 
-                }
-                
-                IsReset = Action.Equals("reset");
-                IsSave = Action.Equals("save");
-                    
-                if ((IsReset || IsSave) && !Type.Equals("all")) {
-                    BackBtnHref = $"Rules?action=list&type={Type}";
-                }
+            if (Request.Query.ContainsKey("reset")) {
+                IsAction = true;
+                Rules.ResetAllToDefault();
+                StatusMsg = "Rules reverted to default values!";
             }
         }
     }
